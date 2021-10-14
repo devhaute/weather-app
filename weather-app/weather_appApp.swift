d78c966a5e3ff6d34004593bee9219b76c2e8d74
devhaute <devhaute@gmail.com>
2021-10-14 16:41:10 +0900

    initial commit

---
+++ weather-app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather-app
+//
+//  Created by devhaute on 2021/10/14.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
