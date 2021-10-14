2bf8b95afa125abe4ad4cc6d821a7d34cfba690b
devhaute <devhaute@gmail.com>
2021-10-14 16:53:29 +0900

    배경화면, 오늘의 날씨 레이아웃 구현

--- weather-app/ContentView.swift
+++ weather-app/ContentView.swift
@@ -9,8 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .topTrailing, endPoint: .bottomTrailing)
+            
+            VStack(spacing: 20){
+                Text("서울")
+                    .font(.system(size: 40))
+                    .foregroundColor(.white)
+                
+                Image(systemName:"sun.max.fill")
+                    .resizable()
+                    .renderingMode(.original)
+                    .frame(width: 180, height: 180)
+                    .scaledToFit()
+                
+                Text("72")
+                    .font(.system(size: 70))
+                    .foregroundColor(.white)
+                
+                Spacer()
+            }
             .padding()
+        }
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
